1c353c3599276126b570ff8ab4c2b4f435c6eb5d
MrChikip <mrchikip@users.noreply.github.com>
2023-01-16 22:52:01 -0500

    generacion de funciones en rutas y controlador

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -1,3 +1,23 @@
 export const getTasks = (req, res) => {
     res.send('Hola Putos!')
 }
+
+export const getTask = (req, res) => {
+    res.send('Hola Putos!')
+}
+
+export const getTaskCount = (req, res) => {
+    res.send('Hola Putos!')
+}
+
+export const saveTask = (req, res) => {
+    res.send('Hola Putos!')
+}
+
+export const deleteTask = (req, res) => {
+    res.send('Hola Putos!')
+}
+
+export const updateTask = (req, res) => {
+    res.send('Hola Putos!')
+}
--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,18 +1,18 @@
 import { Router } from 'express';
-import { getTasks } from '../controllers/tasks'
+import { deleteTask, getTask, getTaskCount, getTasks, saveTask, updateTask } from '../controllers/tasks'
 
 const router = Router()
 
 router.get('/tasks', getTasks)
 
-router.get('/tasks/count')
+router.get('/tasks/count', getTaskCount)
 
-router.get('/tasks:id')
+router.get('/tasks:id', getTask)
 
-router.post('/tasks')
+router.post('/tasks', saveTask)
 
-router.delete('/tasks:id')
+router.delete('/tasks:id', deleteTask)
 
-router.put('/tasks:id')
+router.put('/tasks:id', updateTask)
 
 export default router
