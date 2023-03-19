ce7f91dc29a9ca9aeea59086360fe995822a18cd
MrChikip <mrchikip@users.noreply.github.com>
2023-03-19 00:29:08 -0500

    nuevas tablas en la base de datos

--- src/scripts/db.sql
+++ src/scripts/db.sql
@@ -12,3 +12,45 @@ CREATE TABLE IF NOT EXISTS tasks(
 INSERT INTO tasks (tittle, description) VALUES
     ('Task 1', 'Some Description'),
     ('Task 2', 'Some Description 2');
+
+CREATE TABLE usuarios (
+  nombre_empleado VARCHAR(255),
+  usuario VARCHAR(50),
+  cedula NUMERIC(20,0) PRIMARY KEY,
+  cargo VARCHAR(100),
+  proceso VARCHAR(100),
+  centro_costo VARCHAR(50)
+);
+
+CREATE TABLE equipos (
+  nombre_equipo VARCHAR(50),
+  categoria VARCHAR(50),
+  serial VARCHAR(20) PRIMARY KEY,
+  sistema_operativo VARCHAR(50),
+  modelo VARCHAR(50),
+  fabricante VARCHAR(50),
+  procesador VARCHAR(50),
+  ram VARCHAR(50),
+  tipo_almacenamiento VARCHAR(50),
+  capacidad_almacenamiento VARCHAR(50),
+  licencia_office VARCHAR(50),
+  licencia_adobe VARCHAR(50),
+  licencia_autocad VARCHAR(50),
+  otras_licencias VARCHAR(100),
+  licencia_sap VARCHAR(50),
+  propiedad VARCHAR(50),
+  salud VARCHAR(50),
+  fecha_ultimo_mantenimiento DATE
+);
+
+CREATE TABLE gestion_equipos (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  usuario_cedula NUMERIC(20,0),
+  equipo_serial VARCHAR(20),
+  fecha_movimiento DATE,
+  tipo_movimiento VARCHAR(50),
+  ubicacion VARCHAR(100),
+  link_acta VARCHAR(255),
+  FOREIGN KEY (usuario_cedula) REFERENCES usuarios(cedula),
+  FOREIGN KEY (equipo_serial) REFERENCES equipos(serial_equipo)
+);
